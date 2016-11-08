2778e86cad96b8ff480b380fe19c648a5f9b2575
Václav Šír <vaclav.sir@gmail.com>
2016-11-08 14:27:12 +0100

    Import Firebase

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+import "firebase/storage";
 
 ReactDOM.render(
 	<App />,
 	document.getElementById('root')
 );
+
+const config = {
+	apiKey: "AIzaSyCJyx4AZkIvFJ68HrXjq6yXxLry-ZYN-lw",
+	authDomain: "rock-paper-19fc0.firebaseapp.com",
+	databaseURL: "https://rock-paper-19fc0.firebaseio.com",
+	storageBucket: "rock-paper-19fc0.appspot.com"
+};
+firebase.initializeApp(config);
